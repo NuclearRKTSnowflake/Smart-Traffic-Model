a9e8fc6901125a4289c9f7baed41aa6333ae4f6f
Sierra Knowles <cknowles@q.com>
2015-02-06 12:51:23 -0700

    Added Development Journal and started code

---
+++ Smart Traffic Model/SmartTrafficModel.java
@@ -0,0 +1,14 @@
+/**
+ * This class represents a Smart Traffic Model.
+ *
+ * The Smart Traffic Model will:
+   1. Increase traffic flow.
+   2. Decrease fuel usage.
+   3. Increase safety.
+   4. Decrease commuting time.
+ *  
+ * @author Sierra Knowles
+ * @version 1
+ */
+public class SmartTrafficModel{
+}
