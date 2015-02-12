7f2059c9ac2e8e8f5ece8d495443b03b555b94da
unknown <100903677@R20802278.boiseschools.org>
2015-02-12 13:00:35 -0700

    A WHOLE BUNCH OF WORK

---
+++ Smart Traffic Model Code/SmartTrafficModel.java
@@ -0,0 +1,53 @@
+import java.util.Timer;
+/**
+ * This class represents a Smart Traffic Model.
+ *
+ * The Smart Traffic Model will:
+1. Increase traffic flow.
+2. Decrease fuel usage.
+3. Increase safety.
+4. Decrease commuting time.
+ *  
+ * @author Sierra Knowles
+ * @version 1
+ */
+public class SmartTrafficModel{
+    private int numberOfCars;
+    private int commutingTime;
+    private int numberOfAccidents;
+    private int milesPerGallon;
+    /**
+     * This method creates the Smart Traffic Model.
+     * 
+     * @param numCars - The number of cars passing through the stoplight/intersection.
+     * @param comTime - The amount of time it takes for a car to commute from Point A to Point B.
+     * @param numAccidents - The number of accidents at the stoplight/intersection.
+     * @param mpg - The gas mileage (mpg) of the cars passing through the stoplight/intersection. 
+     */
+    public SmartTrafficModel(int numCars, int comTime, int numAccidents, int mpg){
+    }
+
+    /**
+     * This method runs the program.
+     */
+    public static void main(String[] args){
+    }
+
+    /**
+     * This method measures the commuting time that one car takes to commute from Point A to Point B during the scenario/simulation.
+     */
+    public void measureCommutingTime(){
+    }
+
+    /**
+     * This method measures the gas mileage (mpg) of the cars from Point A to Point B during the scenario/simulation.
+     */
+    public void measureGasMileage(){
+    }
+
+    /**
+     * This method measures the number of accidents that occured during the scenario/simulation.
+     */
+    public void measureNumberOfAccidents(){
+    }
+}
