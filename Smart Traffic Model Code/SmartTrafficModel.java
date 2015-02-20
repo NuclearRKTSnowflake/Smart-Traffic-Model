a08b9caed3e7a93a2cef7bcacfd17912b487e775
unknown <100903677@R20802278.boiseschools.org>
2015-02-20 12:42:32 -0700

    Updated Everything

--- Smart Traffic Model Code/SmartTrafficModel.java
+++ Smart Traffic Model Code/SmartTrafficModel.java
@@ -1,4 +1,5 @@
 import java.util.Timer;
+import java.util.Scanner;
 /**
  * This class represents a Smart Traffic Model.
  *
@@ -12,6 +13,7 @@ import java.util.Timer;
  * @version 1
  */
 public class SmartTrafficModel{
+    static Scanner kbReader = new Scanner(System.in);
     private int numberOfCars;
     private int commutingTime;
     private int numberOfAccidents;
@@ -25,12 +27,52 @@ public class SmartTrafficModel{
      * @param mpg - The gas mileage (mpg) of the cars passing through the stoplight/intersection. 
      */
     public SmartTrafficModel(int numCars, int comTime, int numAccidents, int mpg){
+        numCars = numberOfCars;
+        comTime = commutingTime;
+        numAccidents = numberOfAccidents;
+        mpg = milesPerGallon;
     }
 
     /**
      * This method runs the program.
      */
     public static void main(String[] args){
+        String input = "";
+        boolean run = true;
+        System.out.println("Welcome to the Smart Traffic Model");
+        System.out.println("Please select the intensity of traffic.");
+        System.out.println("Your choices are: light, moderate, heavy");
+        input = kbReader.nextLine();
+        while(run == true){
+            System.out.println("");
+            input = kbReader.nextLine();
+            System.out.println("");
+            readCommand(input);
+        }
+    }
+
+    /**
+     * This method parses and runs the user's command.
+     * 
+     * @param command - The comand the user types in String form.
+     */
+    public static void readCommand(String command){
+        //Code to read commands
+        if(Variables.input.equalsIgnoreCase("light")){
+            //Call method
+        }
+        else if(Variables.input.equalsIgnoreCase("moderate")){
+            //Call method
+        }
+        else if(Variables.input.equalsIgnoreCase("heavy")){
+            //Call method
+        }
+        else{
+            System.out.println("");
+            System.out.println("Sorry, the Smart Traffic Model is unable to interpret your input.");
+            System.out.println("Please try again");
+            System.out.println("");
+        }
     }
 
     /**
---
+++ Smart Traffic Model Code/Variables.java
@@ -0,0 +1,10 @@
+/**
+ * This class stores some of the variables in the Smart Traffic Model class(es).
+ * 
+ * @author Sierra Knowles
+ */
+public class Variables{
+    //SmartTrafficModel class
+    public static boolean run = true;
+    public static String input = "";
+}
